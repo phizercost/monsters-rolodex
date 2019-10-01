b467ba5d60b070db9151d7605c147660307c24bd
phizercost <phizer.development@gmail.com>
2019-10-01 22:57:05 +0200

    Adding card, card-list, search components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -15,11 +18,20 @@ class App extends Component {
       .then(users => this.setState({monsters: users}));
   }
   render() {
+    const { monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+
+      <SearchBox
+      placeholder='search monsters' 
+      handleChange= { e => 
+        this.setState({searchField: e.target.value})}
+      />
+      <CardList monsters={filteredMonsters}/>
+        
       </div>
     );
   }
