61c63542a5b0ed34f147cac0a0eb3209ca82ee39
phizercost <phizer.development@gmail.com>
2019-10-02 12:39:15 +0200

    Finalizing search box

--- src/App.js
+++ src/App.js
@@ -17,6 +17,10 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({monsters: users}));
   }
+
+  onSearchChange =  event => {
+    this.setState({ searchField: event.target.value });
+  };
   render() {
     const { monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster => 
@@ -24,12 +28,8 @@ class App extends Component {
       );
     return (
       <div className="App">
-
-      <SearchBox
-      placeholder='search monsters' 
-      handleChange= { e => 
-        this.setState({searchField: e.target.value})}
-      />
+      <h1> Monsters Rolodex </h1>
+      <SearchBox onSearchChange = { this.onSearchChange } />
       <CardList monsters={filteredMonsters}/>
         
       </div>
